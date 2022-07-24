afab60724346222d179cc672b80a69baf18a9003
Jacob Ross <jacob.ross98@outlook.com>
2022-07-23 20:27:56 -0400

    added prune command

--- commands/prune.js
+++ commands/prune.js
@@ -9,21 +9,26 @@ module.exports = {
 				.setDescription('The amount of messages to delete')
 				.setRequired(true)),
 	async execute(interaction) {
-		const numberToDelete = interaction.options.getInteger('amount');
-		const MAX_DEPTH = 5;
-		let usersMsgs = (await interaction.channel.messages.fetch({ limit: 100 })).filter(m => m.author.id === interaction.user.id)
-		let toDelete = usersMsgs.clone()
-		let lastMsg = usersMsgs.last().id;
-		if(!lastMsg) return;
-		let curr = 1
-		while (usersMsgs.size == 100 && curr !== MAX_DEPTH) {
-			usersMsgs = (await interaction.channel.messages.fetch({ limit: 100, before: lastMsg })).filter(m => m.author.id === interaction.user.id)
-			lastMsg = usersMsgs.last().id
-			curr++;
-			toDelete = toDelete.concat(usersMsgs)
+		try{
+			await interaction.deferReply({ ephemeral: true });
+			const numberToDelete = interaction.options.getInteger('amount');
+			if(numberToDelete > 100) interaction.editReply({ content: "Your amount is bigger than 100", ephemeral: true});
+
+			let messages = (await interaction.channel.messages.fetch({ limit: 100 })).filter(m => m.author.id === interaction.user.id)
+
+			if(messages.size > numberToDelete){
+				const stopVal = messages.size - numberToDelete
+				for(let i = 0; i < stopVal; i++){
+					messages.delete(messages.lastKey())
+				}
+			}
+
+			await interaction.channel.bulkDelete(messages)
+			
+			// await interaction.channel.bulkDelete(msgs)
+			await interaction.editReply({ content: `Deleted ${0} messages`, ephemeral: true });
+		}catch(e){
+			await interaction.editReply({ content: `Error completing command, please try again.`, ephemeral: true });
 		}
-		console.log(toDelete)
-		// await interaction.channel.bulkDelete(msgs)
-		await interaction.reply({ content: `Deleted ${0} messages`, ephemeral: true });
 	},
 };
