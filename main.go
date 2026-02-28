1072d9b7998862539e77cc4b2391348186f3ac41
Jacob Ross <11012027+jross263@users.noreply.github.com>
2026-02-28 11:47:44 -0500

    feat: add .env loading

--- main.go
+++ main.go
@@ -5,10 +5,20 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	log.Println("App started!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env variables: ", err)
+	}
+
+	log.Print("App started!")
+
+	test := os.Getenv("TEST")
+	log.Print(test)
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
