94b11eb7e3d4668a2522901bde743991a6396e67
Jacob Ross <11012027+jross263@users.noreply.github.com>
2026-02-28 14:36:59 -0500

    feat: add discordgo

--- main.go
+++ main.go
@@ -4,20 +4,43 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
 
-	log.Print("App started!")
+	token := os.Getenv("DISCORD_TOKEN")
 
-	test := os.Getenv("TEST")
-	log.Print(test)
+	if strings.TrimSpace(token) == "" {
+		log.Fatal("Discord token not found...")
+	}
+
+	session, _ := discordgo.New("Bot " + token)
+
+	log.Print("Attempting to start bot session...")
+
+	err := session.Open()
+
+	if err != nil {
+		log.Fatal("An error occurred while opening the session: ", err)
+	}
+
+	log.Print("TacoBot running!")
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-stop
+
+	log.Print("Attempting to stop bot session...")
+
+	err = session.Close()
+
+	if err != nil {
+		log.Fatal("An error occurred while closing the session: ", err)
+	}
 }
