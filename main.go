798b17776e0577d7256ca41ab7d4ae6a2e3d3e82
Jacob Ross <11012027+jross263@users.noreply.github.com>
2026-02-28 11:38:21 -0500

    feat: block until interrupt

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	log.Println("App started!")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-stop
 }
